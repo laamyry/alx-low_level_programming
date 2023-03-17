971a75a5b4e68b6d2a21b8d527cfc073aa5a616a
laamyry <slamiri10@gmail.com>
2023-03-17 14:56:28 -0700

    laamyry

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+int x, y;
+
+for (x = 48 ; x <= 57 ; x++)
+{
+
+putchar (x);
+
+}
+for (x = 97 ; x <= 102 ; x++)
+{
+
+putchar (x);
+
+}
+putchar ('\n');
+return (0);
+
+
+}
