3c6006dfd9d8023d6056e8206198369d5217bb08
laamyry <slamiri10@gmail.com>
2023-03-17 14:29:53 -0700

    laamyry

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+	int main()
+{
+	int ch = 122;
+
+	while (ch >= 97)
+	{
+		putchar (ch);
+		ch--;
+	}
+		putchar ('\n');
+	return 0;
+}
