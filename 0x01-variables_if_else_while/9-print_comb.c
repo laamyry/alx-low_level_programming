4983d2910ef2bab3c00a46173abfb6a4a927aa2c
laamyry <slamiri10@gmail.com>
2023-03-17 15:19:48 -0700

    laamyry

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar (i);
+	if (i == 57)
+
+	{
+		continue;
+
+	}
+		putchar (',');
+		putchar (' ');
+
+	}
+
+		putchar ('\n');
+	return (0);
+
+
+}
