499818784f1760482e056c70b434d83884443b6d
laamyry <slamiri10@gmail.com>
2023-03-17 11:42:55 -0700

    laamyry

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 {
 int ch = 97;
 
-while (ch <= 1222)
+while (ch <= 122)
 {
 	if (ch == 101 || ch == 113)
 	{
@@ -22,8 +22,9 @@ while (ch <= 1222)
 	putchar (ch);
 	ch++;
 }
+	putchar ('\n');
 
-return (0);
+	return (0);
 
 
 
