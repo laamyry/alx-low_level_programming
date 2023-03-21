8bd308beb75d84c62c06c11578474833d4e50623
laamyry <slamiri10@gmail.com>
2023-03-21 09:31:34 -0700

    laamyry

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer
+ *
+ */
+
+int _abs(int i)
+{
+	if(i >= 0)
+	{
+		return (i);
+	}
+	else 
+	{
+		return (i * -1);
+	}
+
+
+}
+
