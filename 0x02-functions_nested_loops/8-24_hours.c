10a4c449b30d93b4b3b9986ca84059177b228196
laamyry <slamiri10@gmail.com>
2023-03-21 14:02:00 -0700

    laamyry

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,21 +10,21 @@ void jack_bauer(void)
 
 	int a, b, c, d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b < 4; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c < 6; c++)
+			for (c = 0; c <=56; c++)
 			{
-				for (d = 0; d < 10; d++)
+				for (d = 0; d <= 9; d++)
 				{
 
-					if (a == 2 && b == 3 && c == 5 && d == 9)
+					if (a >= 2 && b >= 4)
 						break;
 
 					_putchar (a + 48);
 					_putchar (b + 48);
-					_putchar (':');
+					_putchar (58);
 					_putchar (c + 48);
 					_putchar (d + 48);
 					_putchar ('\n');
