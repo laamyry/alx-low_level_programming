658785d9028db2c7e85a87ca07e51bd20f47193e
laamyry <slamiri10@gmail.com>
2023-03-21 08:44:54 -0700

    laamyry

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: var
+ *
+ * Return: 1 for greater than zero. 0 for zero. -1 for less than zero.
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+
+
+
+
+}
