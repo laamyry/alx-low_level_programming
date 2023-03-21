0373a15a60b5863bc149e6378cce6543d6334642
laamyry <slamiri10@gmail.com>
2023-03-21 05:27:09 -0700

    laamyry

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+
 	_putchar ('_');
 	_putchar ('p');
 	_putchar ('u');
@@ -17,7 +17,9 @@ int main(void)
 	_putchar ('r');
 	_putchar ('\n');
 
-return (0);
+	return (0);
+
+
 
 
 
