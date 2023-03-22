95c98e0217f9c118c409cf417c8c5f2589c8be9a
laamyry <slamiri10@gmail.com>
2023-03-22 16:43:11 -0700

    laamyry

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -3,11 +3,9 @@
 /**
  * convert_day - converts day of month to day of year, without accounting
  * for leap year
- *
  * @month: month in number format
  * @day: day of month
- *
- * Return: days of the year
+ * Return: day of year
  */
 
 int convert_day(int month, int day)
@@ -52,3 +50,4 @@ int convert_day(int month, int day)
 	}
 	return (day);
 }
+
