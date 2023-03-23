1637c7e848957e6d5b685dbb6c1871c13a1260c8
laamyry <slamiri10@gmail.com>
2023-03-23 06:47:12 -0700

    laamyry

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters.
+ * @c : input char.
+ *
+ * Return: Returns 1 if c is uppercase and Returns 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+
+		return (0);
+	}
+	else
+		return (1);
+
+}
