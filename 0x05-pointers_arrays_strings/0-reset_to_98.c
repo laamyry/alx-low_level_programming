390044d795ade16af1b2c8973e4883cadba85893
laamyry <slamiri10@gmail.com>
2023-03-27 10:18:17 -0700

    laamyry

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - parameter and updates the value it points to to 98.
+ *
+ * @n: value.
+ * Return: return value.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
