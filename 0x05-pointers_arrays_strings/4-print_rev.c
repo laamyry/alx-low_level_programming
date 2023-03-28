c185465e568199aa24caba77ddb5c7dbba43e676
laamyry <slamiri10@gmail.com>
2023-03-27 18:29:32 -0700

    laamyry

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @str: string value.
+ */
+
+void print_rev(char *s)
+{
+	int le = 0;
+	int n;
+
+	while (*s != '\0')
+	{
+		le++;
+		s++;
+	}
+	
+	s--;
+	for (n = le; n > 0; n--)
+	{
+	_putchar (*s);
+	s--;
+	}
+	_putchar('\n');
+}
