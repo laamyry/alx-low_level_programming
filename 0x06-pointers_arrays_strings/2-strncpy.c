41a4ed75c37c20d12e0f094b0e8f2a22aee6ccf0
laamyry <slamiri10@gmail.com>
2023-03-30 16:10:30 -0700

    laamyry

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string.
+ * @dest: string value.
+ * @src: string value.
+ * @n: number of.
+ * Return: return to _strncpy.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+
+
+}
