031588e53a143b7337c14f540e5ddde342051e03
laamyry <slamiri10@gmail.com>
2023-03-30 15:39:09 -0700

    laamyry

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings.
+ * @n: numbers of bytes.
+ * @src: string value.
+ *@dest: string value.
+ * Return: return string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\n')
+	{
+	i++;
+	}
+
+
+
+
+
+}
