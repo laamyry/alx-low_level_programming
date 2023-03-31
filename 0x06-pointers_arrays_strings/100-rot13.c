7a60948debecd0351630800197f75075b8fd1a41
laamyry <slamiri10@gmail.com>
2023-03-30 19:59:57 -0700

    laamyry

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @n: input value.
+ * Return: return to rot13.
+ */
+char *rot13(char *n)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		if (*p >= 'a' && *p <= 'z')
+		{
+			*p = (*p - 'a' + 13) % 26 + 'a';
+		}
+		else if (*p >= 'A' && *p <= 'Z')
+		{
+			*p = (*p - 'A' + 13) % 26 + 'A';
+		}
+		p++;
+	}
+	return (str);
+
+}
