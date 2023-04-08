6716d101bd4cda5d864bc6359672256a8361bc90
laamyry <slamiri10@gmail.com>
2023-04-08 01:06:21 +0000

    laamyry

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * main - main function.
+ *
+ * Return: void -0.
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
