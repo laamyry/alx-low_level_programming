66286094ea859369a446ba1d5fbda89bc90f477a
laamyry <slamiri10@gmail.com>
2023-04-28 00:01:50 +0000

    med

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
  * list_len - returns the number of elements.
- * 
+ *
  * @h: list_t list.
  * Return: elements number.
 */
 size_t list_len(const list_t *h)
 {
 
-    size_t n;
+	size_t n;
 
-    for(n = 0; h; n++)
-    {
+	for (n = 0; h; n++)
+	{
 
-        h = h->next;
-    }
-    return(n);
+		h = h->next;
+	}
+	return (n);
 
 }
