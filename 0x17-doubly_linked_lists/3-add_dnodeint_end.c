342aeeb60f20acd3ffdd563576e1a67bd124e84f
laamyry <slamiri10@gmail.com>
2023-06-15 22:24:58 +0100

    laamyry py

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ *
+ * @head: the list head.
+ * @n: value.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
+	dlistint_t *current = *head;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+    {
+		return (NULL);
+    }
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (current == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL)
+    {
+		current = current->next;
+    }
+	current->next = new_node;
+	new_node->prev = current;
+
+	return (new_node);
+
+}
