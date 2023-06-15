3a0283e4fbcdd5998d369e80a0ed44b5d18ce1e3
laamyry <slamiri10@gmail.com>
2023-06-15 22:26:18 +0100

    laamyry py

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,9 +13,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	new_node->n = n;
 	new_node->next = NULL;
 
@@ -27,9 +27,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	while (current->next != NULL)
-    {
+	{
 		current = current->next;
-    }
+	}
 	current->next = new_node;
 	new_node->prev = current;
 
