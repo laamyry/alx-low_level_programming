c9875342439b103fe226b63a5fa6f747421647e7
laamyry <slamiri10@gmail.com>
2023-06-15 16:35:38 +0100

    laamyry py

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,12 +2,14 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @head: pointer to head pointer of the list
- * @n: value of new node
- * Return: address of new node or NULL if it fails
+ * 
+ * @head: head pointer.
+ * @n: new node
+ * Return: the address of the new element, or NULL if it failed.
  **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
@@ -25,4 +27,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	*head = new_node;
 
 	return (new_node);
+	
 }
