56865aa1f2b400a6a97c7de8a616b930968e1af0
laamyry <slamiri10@gmail.com>
2023-07-07 22:32:58 +0100

    laamyry py

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * key_index -  gives you the index of a key.
+ * key_index - gives you the index of a key.
  *
  * @key: The key.
  * @size: the size of the array of the hash table.
