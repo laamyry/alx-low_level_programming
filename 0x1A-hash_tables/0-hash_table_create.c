032f623ef9313b12f92efd23dd1629e77c2b1c13
laamyry <slamiri10@gmail.com>
2023-07-08 02:00:45 +0100

    laamyry py

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,37 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - creates a hash table.
+ * hash_table_create - creates a hash tbl.
  *
- * @size: the size of the array.
- * Return: a pointer to the newly created hash table
+ * @size: the size of the arr.
+ * Return: a pointer to the newly created hash tbl
  * If something went wrong, your function should return NULL.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int m;
+	hash_table_t *tbl;
+	hash_node_t **arr;
+	unsigned long int m = 0;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	tbl = malloc(sizeof(hash_table_t));
+	if (tbl == NULL)
 	{
 		return (NULL);
 	}
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	for (m = 0; m < size; m++)
+
+	while (m < size)
 	{
-		ht->array[m] = NULL;
+		arr[m] = NULL;
+		m++;
 	}
 
-	return (ht);
+	tbl->array = arr;
+	tbl->size = size;
+
+	return (tbl);
 }
