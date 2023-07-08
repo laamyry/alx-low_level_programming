6ca2ebf30316df288b16ea41149283ba89815579
laamyry <slamiri10@gmail.com>
2023-07-08 01:55:46 +0100

    laamyry py

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,28 +8,27 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
+	unsigned long int m;
 
-	unsigned long int m = 0;
-	hash_table_t *hash_table;
-
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 	{
 		return (NULL);
 	}
 
-	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 	{
 		return (NULL);
 	}
+	m = 0;
 	while (m < size)
 	{
-		hash_table->array[m] = NULL;
 		m++;
+		ht->array[m] = NULL;
 	}
 
-	return (hash_table);
-
+	return (ht);
 }
