a5e5c665dde2df2cce35fa5797cca4b4fc06c39e
laamyry <slamiri10@gmail.com>
2023-10-09 16:35:39 +0100

    0x1E-search_algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef _search_algos_h_
+#define _search_algos_h_
+
+#include <stdio.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
